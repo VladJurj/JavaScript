c258c327404bd79e797dbf0e88b3cdd14ae51039
yourusername <jurjvlad97@gmail.com>
2023-11-08 17:33:00 +0200

    ex2

--- ex.js
+++ ex.js
@@ -3,3 +3,7 @@ const number2 = 22;
 const sum = number1 + number2;
 console.log(`The sum is: ${sum}`);
 
+const number1 = 5;
+const number2 = 3;
+const number3 = 2;
+console.log(Math.max(1,2,3)); // 3
