d05de12d3e9de36655e578ffaa4f3d372facf5fe
yourusername <jurjvlad97@gmail.com>
2023-11-08 17:38:01 +0200

    ex5

--- ex.js
+++ ex.js
@@ -11,3 +11,18 @@ console.log(Math.max(1,2,3)); // 3
 let variableX = 30;
 variableX = variableX + 5;
 console.log(`The value of variableX is now: ${variableX}`);
+
+
+
+
+
+
+
+const valueA = 3;
+const valueB = 7;
+
+if (valueA > valueB) {
+  console.log("valueA is greater than valueB");
+} else {
+  console.log("valueB is greater than or equal to valueA");
+}
